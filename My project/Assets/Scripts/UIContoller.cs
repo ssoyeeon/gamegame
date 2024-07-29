f6b05cae973079afbabd84bf4525a853f3b65748
ssoyeeon <ssuu2039@gmail.com>
2024-07-29 12:44:14 +0900

    [feat]버튼 스크립트... 상점,집 버튼으로 바꿈요

--- My project/Assets/Scripts/UIContoller.cs
+++ My project/Assets/Scripts/UIContoller.cs
@@ -7,6 +7,7 @@ public class UIContoller : MonoBehaviour
 {
     public GameManager gm;                          //공부해야됨
     public TextMeshProUGUI timeUI;
+    public TextMeshProUGUI hpUI;
 
     void Start()
     {
@@ -18,5 +19,7 @@ public class UIContoller : MonoBehaviour
     {
         timeUI.text = gm.time.ToString("00") + ":" + gm.minute.ToString("00");                      //UI에 출력 
         //Tostring()의 괄호 안에 "00"을 넣어주면 10의 자리까지 출력가능
+        hpUI.text = gm.playerHP.ToString("00");
+        //Hp 띄울거양잉 
     }
 }
