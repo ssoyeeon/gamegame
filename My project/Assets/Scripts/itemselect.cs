a7016cd93122936d9c987d9b7e1b3b791dae8559
gy1022 <bokowife2@naver.com>
2024-07-30 15:57:13 +0900

    [Fix] 오류

--- My project/Assets/Scripts/itemselect.cs
+++ My project/Assets/Scripts/itemselect.cs
@@ -3,47 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class itemselect : MonoBehaviour
-{ 
+{
     //좌클릭 우클릭하면 수확하기
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("씨앗을 심었습니다")
+            Debug.Log("씨앗을 심었습니다");
         }
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("작물을 수확했습니다")
+            Debug.Log("작물을 수확했습니다");
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Hoe();
             Debug.Log("호미가 선택되었습니다");
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            WateringPot();
             Debug.Log("물뿌리개가 선택되었습니다");
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            Seed();
-            Debug.Log("E key pressed")
+            Debug.Log("E key pressed");
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
-            Seed();
             Debug.Log("R key pressed");
         }
         else if(Input.GetKeyDown(KeyCode.D))
         {
-            Seed();
             Debug.Log("D key pressed");
         }
         else if(Input.GetKeyDown(KeyCode.F))
         {
-            Seed();
             Debug.Log("F key pressed");
         }
     }
